9a199c1ed5a372efe306e5d174693ddc31fe1e7c
Antonis Maronikolakis <antmarakis@programmers.gr>
2017-02-12 18:00:14 +0200

    Multi-layered implementation
    Previously we only had one hidden layer. Now we can add as many as we want.

--- Classifiers/Neural Network/NeuralNetwork.py
+++ Classifiers/Neural Network/NeuralNetwork.py
@@ -2,6 +2,61 @@ import numpy as np;
 import Reader;
 
 
+###_Evaluation Functions_###
+def Accuracy(X, Y, Weights, layers):
+    layers += 1;
+    
+    #Run training set through network, find overall accuracy
+    correct = 0;
+
+    for i in range(len(X)):
+        x = X[i];
+        y = list(Y[i].A1);
+
+        guess = Predict(x, Weights, layers);
+
+        if(y == guess):
+            #Guessed correctly
+            correct += 1;
+
+    return correct / float(len(X));
+
+def K_FoldValidation(k, X, Y, f, hiddenLayers, nodes, epochs, r=0.15):
+    if (k > len(X)):
+        return -1;
+    
+    correct = 0;  #The number of correct classifications
+    total = len(X) * (k - 1);  #The total number of classifications
+
+    l = len(X) / k;  #The length of a fold
+
+    for i in range(k):
+        print "\nFold",i;
+        
+        #Split data set into training and testing
+        trainingX = X[i * l:(i + 1) * l];
+        trainingY = Y[i * l:(i + 1) * l];
+
+        testX = np.concatenate([X[:i*l],X[(i+1)*l:]]);
+        testY = np.concatenate([Y[:i*l],Y[(i+1)*l:]]);
+
+        #Calculate Weights
+        weights = NeuralNetwork(epochs,X,Y,f,hiddenLayers,nodes,r);
+
+        #Make predictions for test sets
+        for j in range(len(testX)):
+            x = testX[j];
+            y = list(testY[j].A1);
+
+            guess = Predict(x, weights, hiddenLayers+1);
+
+            if(y == guess):
+                #Guessed correctly
+                correct += 1;
+
+    return correct / float(total);
+
+
 ###_Auxiliary Functions_###
 def Sigmoid(x):
     return 1 / (1 + np.exp(-x));
@@ -9,98 +64,46 @@ def Sigmoid(x):
 def SigmoidDerivative(x):
     return np.multiply(x, 1 - x);
 
-def InitializeWeights(f, hidden, output):
-    #Initialize weights with random values in [-1,1] (including bias)
+def InitializeWeights(f, layers, nodes):
+    ##_Initialize weights with random values in [-1,1] (including bias)_##
 
     #Augment feature vectors with bias
     f += 1;
 
-    #Initialize second (hidden) layer weights
-    W1 = [[np.random.uniform(-1, 1) for i in range(f)] for j in range(hidden)];
-    W1 = np.matrix(W1);
+    #Initialize weights from input to first hidden layer
+    inputToHidden = [[np.random.uniform(-1, 1) for i in range(f)] for j in range(nodes[0])];
+    inputToHidden = np.matrix(inputToHidden);
 
-    #Initialize third (output) layer weights
-    W2 = [[np.random.uniform(-1, 1) for i in range(hidden + 1)] for j in range(output)];
-    W2 = np.matrix(W2);
+    weights = [inputToHidden];
+    #Initialize the rest of the weights
+    for i in range(1,layers):
+        w = [[np.random.uniform(-1, 1) for k in range(nodes[i-1] + 1)] for j in range(nodes[i])];
+        w = np.matrix(w);
+        weights.append(w);
 
-    return W1, W2;
+    return weights;
 
 
 ###_Core Functions_###
-def Train(X, Y, f, hidden, output, W1, W2, r):
-    for i in range(len(X)):
-        x = X[i];
-        y = Y[i].A1;
-        x = np.matrix(np.append(1, x));  # Augment feature vector
+def Predict(item, Weights, layers, sigmoid=True):
+    item = np.append(1, item);  #Augment feature vector
 
-        ##_Forward Propagation_##
+    ##_Forward Propagation_##
+    activations = [item];
+    Input = item;
+    for i in range(layers):
+        activation = np.dot(Input, Weights[i].T);
+        if(i < layers-1 or sigmoid):
+            #When calculating the output activation, check if
+            #we should sigmoid it or not (via the sigmoid var)
+            activation = Sigmoid(activation);
+        
+        activations.append(activation);
 
-        #Hidden layer activation
-        activationHidden = Sigmoid(np.dot(x,W1.T));
+        Input = np.append(1, activation); #Augment activation vector
+    
+    outputFinal = activations[-1].A1;
 
-        #Augment hidden activation output
-        augmentedHidden = np.append(1, activationHidden);
-        outputFinal = Sigmoid(np.dot(augmentedHidden,W2.T));
-
-        ##_Backpropagation_##
-
-        #_Correction for Output_#
-
-        #Convert output activation to matrix
-        aO = np.matrix(outputFinal);
-
-        errorOutput = np.matrix(y - outputFinal);  #Prediction error
-        deltaOutput = np.multiply(errorOutput, SigmoidDerivative(aO));
-
-        #Convert hidden activation to matrix
-        aA = np.matrix(augmentedHidden);
-        D = r*np.multiply(deltaOutput.T, aA);  #Correction for output layer
-
-        W2 += D;  #Correct synapses/weights between hidden-output layers
-
-        #_Correction for Hidden_#
-        w = np.delete(W2, [0], axis=1);  #remove bias
-
-        #Convert hidden activation (without bias) to matrix
-        aH = np.matrix(activationHidden);
-
-        #Propagate error backwards
-        errorHidden = np.dot(deltaOutput, w);
-        deltaHidden = np.multiply(errorHidden, SigmoidDerivative(aH));
-
-        DH = r*np.multiply(deltaHidden.T, x);  #Correction for hidden layer
-
-        W1 += DH;  #Correct synapses/weights between input-hidden layers
-
-    return W1, W2;
-
-def NeuralNetwork(epochs, X, Y, r, f, hidden, output):
-    W1, W2 = InitializeWeights(f, hidden, output);
-
-    for epoch in range(epochs):
-        #Train weights
-        W1, W2 = Train(X, Y, f, hidden, output, W1, W2, r);
-
-        if (epoch % 25 == 0):
-            print "Epoch ", epoch;
-
-    return W1, W2;
-
-
-###_Evaluation Functions_###
-def Predict(item, Weights, hidden, output):
-    W1, W2 = Weights;
-
-    item = np.append(1, item);  # Augment feature vector
-
-    #_Forward Propagation_#
-    outputHidden = Sigmoid(np.dot(item,W1.T));
-    #Augment hidden activation output
-    outputHidden = np.append(1, outputHidden);
-    outputFinal = Sigmoid(np.dot(outputHidden,W2.T));
-    #outputFinal = LayerActivation(output, W2, outputHidden);
-
-    outputFinal = outputFinal.A1;
     #Find max activation in output
     m = outputFinal[0];
     index = 0;
@@ -117,54 +120,71 @@ def Predict(item, Weights, hidden, output):
 
     return y;  #Return prediction vector
 
-def Accuracy(X, Y, Weights, hidden, output):
-    #Run training set through network, find overall accuracy
-    correct = 0;
-
+def Train(X, Y, r, layers, weights):
     for i in range(len(X)):
         x = X[i];
-        y = list(Y[i].A1);
-
-        guess = Predict(x, Weights, hidden, output);
-
-        if (y == guess):
-            correct += 1;
-
-    return correct / float(len(X));
-
-def K_FoldValidation(k, r, X, Y, f, h, o, epochs):
-    if (k > len(X)):
-        return -1;
-
-    correct = 0;  #The number of correct classifications
-    total = len(X) * (k - 1);  #The total number of classifications
-
-    l = len(X) / k;  #The length of a fold
-
-    for i in range(k):
-        print "\nNew Fold";
+        y = Y[i].A1;
+        x = np.matrix(np.append(1, x));  # Augment feature vector
         
-        #Split data set into training and testing
-        trainingX = X[i * l:(i + 1) * l];
-        trainingY = Y[i * l:(i + 1) * l];
+        ##_Forward Propagation_##
+        #Each layer receives an input and calculates its output
+        #The output of one layer is the input to the next
+        #The first input is the first feature vector (the item)
+        activations = [x];
+        Input = x;
+        for i in range(layers):
+            activation = Sigmoid(np.dot(Input,weights[i].T));
+            activations.append(activation);
+            
+            Input = np.append(1, activation); #Augment with bias
 
-        testX = np.concatenate([X[:i*l],X[(i+1)*l:]]);
-        testY = np.concatenate([Y[:i*l],Y[(i+1)*l:]]);
+        ##_Back Propagation_##
+        #Find error at output
+        #Propagate error backwards through the layers
+        #For each layer:
+        #a) Calculate delta:
+            #Error of next layer * the sigmoid der of current layer activation
+        #b) Update weights between current layer and previous layer
+            #Multiply delta with activation of previous layer
+            #Multiply that with rate
+            #Add that to weights of previous layer
+        #c) Calculate error for current layer
+            #Remove bias from previous-layer weights, get w
+            #Multiply delta with w to get error
+        outputFinal = activations[-1];
+        error = np.matrix(y - outputFinal); #Error at output
+        
+        for i in range(layers,0,-1):
+            currActivation = activations[i];
+            
+            if(i > 1):
+                #Augment previous activation
+                prevActivation = np.append(1,activations[i-1]);
+            else:
+                #First hidden layer, prevActivation is input (without bias)
+                prevActivation = activations[i-1];
+            
+            delta = np.multiply(error, SigmoidDerivative(currActivation));
+            weights[i-1] = r * np.multiply(delta.T,prevActivation);
 
-        #Calculate Weights
-        W1, W2 = NeuralNetwork(epochs, trainingX, trainingY, r, f, h, o);
+            w = np.delete(weights[i-1], [0], axis=1); #remove bias from weights
+            
+            error = np.dot(delta,w); #Calculate error for curr layer
 
-        #Make predictions for test sets
-        for j in range(len(testX)):
-            x = testX[j];
-            y = list(testY[j].A1);
+    return weights;
 
-            guess = Predict(x, [W1, W2], h, o);
+def NeuralNetwork(epochs, X, Y, f, hiddenLayers, nodes, r=0.15):
+    layers = hiddenLayers + 1; #Total number of layers in network
+    weights = InitializeWeights(f, layers, nodes);
 
-            if (y == guess):
-                correct += 1;
+    for epoch in range(epochs):
+        #Train weights
+        weights = Train(X, Y, r, layers, weights);
 
-    return correct / float(total);
+        if(epoch % 25 == 0):
+            print "Epoch ", epoch;
+
+    return weights;
 
 
 ###_Main_###
@@ -172,15 +192,17 @@ def main():
     X,Y = Reader.ReadData('data.txt');
 
     f = len(X[0].A1);
-    h = 10;
+    h1 = 5;
+    h2 = 10;
     o = len(Y[0].A1);
+    hiddenLayers = 2;
     r = 0.15;
-    epochs = 200;
+    epochs = 100;
 
-    print K_FoldValidation(5, r, X, Y, f, h, o, epochs);
-
-    #W1, W2 = NeuralNetwork(epochs,X,Y,r,f,h,o);
-    #print Accuracy(X, Y, [W1,W2], h, o);
+    print K_FoldValidation(5, X, Y, f, hiddenLayers, [h1,h2,o], epochs, r);
+    
+    #weights = NeuralNetwork(epochs,X,Y,f,hiddenLayers,[h1,h2,o],r);
+    #print Accuracy(X, Y, weights, hiddenLayers);
 
 if __name__ == "__main__":
     main();
